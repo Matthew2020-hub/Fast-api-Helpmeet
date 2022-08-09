03f72e6670b76c06b4d4e9d7229c335d79583c55
Matthew <akinolatolulope24@gmail.com>
2022-08-09 23:27:32 +0100

    email verification  error fixed

--- project/library/database/database.py
+++ project/library/database/database.py
@@ -5,12 +5,13 @@ from fastapi import FastAPI
 from tortoise import Tortoise
 from tortoise.contrib.fastapi import register_tortoise
 
-from Authentication.config import (
-    DATABASE_PASSWORD,
-    DATABASE_DB,
-    DATABASE_HOST,
-    DATABASE_PORT,
-    DATABASE_USER
+from config import (
+    POSTGRES_USER,
+    POSTGRES_PASSWORD,
+    POSTGRES_SERVER,
+    POSTGRES_PORT,
+    POSTGRES_DB,
+    DATABASE_URL,
 )
 
 logger = logging.getLogger(__name__)
@@ -22,11 +23,11 @@ TORTOISE_ORM = {
         "default": {
             "engine": "tortoise.backends.asyncpg",
             "credentials": {
-                "host": f"{DATABASE_HOST}",
-                "port": f"{DATABASE_PORT}",
-                "user": f"{DATABASE_USER}",
-                "password": f"{DATABASE_PASSWORD}",
-                "database": f"{DATABASE_DB}",
+               "host": f"{POSTGRES_SERVER}",
+                "port": f"{POSTGRES_PORT}",
+                "user": f"{POSTGRES_USER}",
+                "password": f"{POSTGRES_PASSWORD}",
+                "database": f"{POSTGRES_DB}",
             },
         },
     },
@@ -38,21 +39,29 @@ TORTOISE_ORM = {
     },
 }
 
+
 async def init_db(app: FastAPI) -> None:
+    """Initialize database."""
     try:
         register_tortoise(
             app,
-            db_url=f"postgres://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_DB}",
+            db_url=DATABASE_URL,
             modules={"models": MODELS},
-            generate_schemas=True,
-            add_exception_handlers=True
+            generate_schemas=False,
+            add_exception_handlers=True,
         )
-        logger.warning("---DB CONNECTION WAS SUCCESSFUL ---")
+        logger.warning("--- DB CONNECTION WAS SUCCESSFUL ---")
     except Exception as e:
         logger.warning("--- DB CONNECTION ERROR ---")
         logger.warning(e)
         logger.warning("--- DB CONNECTION ERROR ---")
 
+
+def create_start_app_handler(app: FastAPI) -> Callable:
+    async def start_app() -> None:
+        await init_db(app)
+
+    return start_app
 
 def create_start_app_handler(app: FastAPI) -> Callable:
     async def start_app() -> None:
